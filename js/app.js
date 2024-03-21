a5c1c1bff3c9000d3a80d90f2d423939167c5f3a
Helmatron <ema.matro@gmail.com>
2024-03-21 19:49:46 +0100

    pausa

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 console.log('Pali&dispari')
+
+// << CONESGNA >>
+
+// << Palidroma >>
+// Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma. Questa funzione deve ricevere come input la parola inserita dall’utente e ritornare true se la parola è palindroma e false se la parola non è palindroma
+
+// << Pari e Dispari >>
+// L’utente sceglie pari o dispari (tramite prompt) e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione provate a scrivervela voi altrimenti copiatevela da mdn).
+// Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+// << Consigli del giorno >>
+// Scriviamo sempre in italiano i passaggi che vogliamo fare
+// Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+// Buon lavoro e buon divertimento! :slightly_smiling_face:
+
+
