948aaca6637db13868e4e2ee69d87b85cae2ade5
Helmatron <ema.matro@gmail.com>
2024-03-25 22:05:03 +0100

    fine: palindroma (grazie alla correzione in classe)

--- js/app.js
+++ js/app.js
@@ -1,11 +1,61 @@
 console.log('Pali&dispari')
 
+
+/* 
 // << CONESGNA >>
 
 // << Palidroma >>
-// Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma. Questa funzione deve ricevere come input la parola inserita dall’utente e ritornare true se la parola è palindroma e false se la parola non è palindroma
+
+// Chiedere all’utente di inserire una parola
+const parola = prompt('inserire una parola')
+console.log(parola)
+
+
+// creo una variabile per inizializzare la funzione
+
+const risultato = isPalindrome(parola)
+
+
+// Creare una funzione per capire se la parola inserita è palindroma.
+
+function isPalindrome(parola) {
+
+    // Variabile per il testo invertito
+    let invertedText = ''
+
+    for (let i = parola.length -1; i >= 0 ; i--) {
+        const cicloParola = parola[i];
+        console.log(cicloParola)
+
+        invertedText += cicloParola
+    }
+
+    console.log(invertedText)
+
+    if (parola === invertedText) {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+// Questa funzione deve ricevere come input la parola inserita dall’utente e ritornare true se la parola è palindroma e false se la parola non è palindroma
+
+if (risultato === true) {
+    console.log('la parola ' + parola + ' è palindroma')
+    
+} else {
+    console.log('La parola ' + parola + ' non è palindroma')
+}
+
+*/
+
+
+
 
 // << Pari e Dispari >>
+
 // L’utente sceglie pari o dispari (tramite prompt) e inserisce un numero da 1 a 5.
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione provate a scrivervela voi altrimenti copiatevela da mdn).
 // Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
